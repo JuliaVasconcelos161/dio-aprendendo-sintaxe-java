bb14863d22615d0b95dd464f94b879d05b2b5a55
Julia <juliavasconceloscr@gmail.com>
2023-01-08 20:32:28 -0300

    Término do módulo Tipos e Variáveis

---
+++ src/com/github/juliavasconcelos161/javasintaxe/tiposvariaveis/TiposVariaveis.java
@@ -0,0 +1,21 @@
+package com.github.juliavasconcelos161.javasintaxe.tiposvariaveis;
+
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+        System.out.println(numero);
+
+        final double VALOR_PI = 5;
+
+    }
+
+
+}
