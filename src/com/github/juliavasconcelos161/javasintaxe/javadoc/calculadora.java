4c585603778d381edc6580e6d0a104cb374cc554
Julia <juliavasconceloscr@gmail.com>
2023-01-08 23:51:15 -0300

    Término modulo JavaDoc

---
+++ src/com/github/juliavasconcelos161/javasintaxe/javadoc/calculadora.java
@@ -0,0 +1,18 @@
+package com.github.juliavasconcelos161.javasintaxe.javadoc;
+/* Calculadora
+*A Calculadora realiza operações matemáticas entre números inteiros
+* @author Júlia Vasconcelos
+* @version 1.0
+* @since 09/01/2023
+* */
+public class calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int é o resultado deste método, a soma dos dois números
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
---
+++ src/com/github/juliavasconcelos161/javasintaxe/javadoc/comentarios.java
@@ -0,0 +1,18 @@
+package com.github.juliavasconcelos161.javasintaxe.javadoc;
+
+public class comentarios {
+    public static void main(String[] args) {
+        //unica linha
+        /*
+        * Muitas
+        * Linhas
+         */
+
+        /**
+         * Documentação
+         */
+
+        //No terminal execute o comando abaixo para criar documentação no formato html
+        //javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d../docs src/*.java
+    }
+}
